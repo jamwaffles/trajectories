66e57c741d2d6c41ab772d8392f7d718c9217812
James Waples <jamwaffles@gmail.com>
2018-10-08 16:41:06 +0100

    Remove duplicate tests, clean up suite

--- trajectories/benches/comparison.rs
+++ trajectories/benches/comparison.rs
@@ -8,64 +8,19 @@ use std::time::Duration;
 use trajectories::{test_helpers::TestCoord3, Path, Trajectory};
 use trajectories_sys::{path_create, Trajectory as CPPTrajectory};
 
-fn cpp_bindings(c: &mut Criterion) {
-    c.bench_function("c++ bindings trajectory create", |b| {
-        b.iter(|| {
-            let max_velocity = [1.0, 1.0, 1.0];
-            let max_acceleration = [1.0, 1.0, 1.0];
-
-            let waypoints: Vec<f64> = vec![
-                0.0, 0.0, 0.0, 0.0, 0.2, 1.0, 0.0, 3.0, 0.5, 1.1, 2.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-                1.0, 0.0, 0.0, 0.0, 1.0,
-            ];
-
-            let path = unsafe { path_create(waypoints.as_ptr(), waypoints.len(), 0.001f64) };
-
-            let _traj =
-                unsafe { CPPTrajectory::new(path, &max_velocity, &max_acceleration, 0.001f64) };
-        })
-    });
-}
-
-fn rust_native(c: &mut Criterion) {
-    c.bench_function("rust native trajectory create", |b| {
-        b.iter(|| {
-            let waypoints: Vec<TestCoord3> = vec![
-                TestCoord3::new(0.0, 0.0, 0.0),
-                TestCoord3::new(0.0, 0.2, 1.0),
-                TestCoord3::new(0.0, 3.0, 0.5),
-                TestCoord3::new(1.1, 2.0, 0.0),
-                TestCoord3::new(1.0, 0.0, 0.0),
-                TestCoord3::new(0.0, 1.0, 0.0),
-                TestCoord3::new(0.0, 0.0, 1.0),
-            ];
-
-            let p = Path::from_waypoints(&waypoints, 0.001);
-
-            let _trajectory = Trajectory::new(
-                p,
-                TestCoord3::new(1.0, 1.0, 1.0),
-                TestCoord3::new(1.0, 1.0, 1.0),
-                0.000001,
-                0.001,
-            );
-        })
-    });
-}
-
 fn compare(c: &mut Criterion) {
     let comp_native = Fun::new("rust native", |b, _| {
-        b.iter(|| {
-            let waypoints: Vec<TestCoord3> = vec![
-                TestCoord3::new(0.0, 0.0, 0.0),
-                TestCoord3::new(0.0, 0.2, 1.0),
-                TestCoord3::new(0.0, 3.0, 0.5),
-                TestCoord3::new(1.1, 2.0, 0.0),
-                TestCoord3::new(1.0, 0.0, 0.0),
-                TestCoord3::new(0.0, 1.0, 0.0),
-                TestCoord3::new(0.0, 0.0, 1.0),
-            ];
+        let waypoints: Vec<TestCoord3> = vec![
+            TestCoord3::new(0.0, 0.0, 0.0),
+            TestCoord3::new(0.0, 0.2, 1.0),
+            TestCoord3::new(0.0, 3.0, 0.5),
+            TestCoord3::new(1.1, 2.0, 0.0),
+            TestCoord3::new(1.0, 0.0, 0.0),
+            TestCoord3::new(0.0, 1.0, 0.0),
+            TestCoord3::new(0.0, 0.0, 1.0),
+        ];
 
+        b.iter(|| {
             let p = Path::from_waypoints(&waypoints, 0.001);
 
             let _trajectory = Trajectory::new(
@@ -78,15 +33,15 @@ fn compare(c: &mut Criterion) {
         })
     });
     let comp_bindings = Fun::new("c++ bindings", |b, _| {
+        let max_velocity = [1.0, 1.0, 1.0];
+        let max_acceleration = [1.0, 1.0, 1.0];
+
+        let waypoints: Vec<f64> = vec![
+            0.0, 0.0, 0.0, 0.0, 0.2, 1.0, 0.0, 3.0, 0.5, 1.1, 2.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+            0.0, 0.0, 0.0, 1.0,
+        ];
+
         b.iter(|| {
-            let max_velocity = [1.0, 1.0, 1.0];
-            let max_acceleration = [1.0, 1.0, 1.0];
-
-            let waypoints: Vec<f64> = vec![
-                0.0, 0.0, 0.0, 0.0, 0.2, 1.0, 0.0, 3.0, 0.5, 1.1, 2.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-                1.0, 0.0, 0.0, 0.0, 1.0,
-            ];
-
             let path = unsafe { path_create(waypoints.as_ptr(), waypoints.len(), 0.001f64) };
 
             let _traj =
@@ -103,13 +58,11 @@ criterion_group!(
     name = create_trajectory;
 
     config = Criterion::default()
-        .warm_up_time(Duration::from_millis(1000))
-        .sample_size(30)
+        .warm_up_time(Duration::from_millis(2000))
+        .sample_size(25)
         .measurement_time(Duration::from_millis(6000));
 
     targets =
-        rust_native,
-        cpp_bindings,
         compare
 );
 
